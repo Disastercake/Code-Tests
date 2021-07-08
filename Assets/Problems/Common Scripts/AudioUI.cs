b07982e87d5e2a7be6186cf559fc9bfb2219f145
Disastercake <mike@disastercake.com>
2021-07-07 21:25:16 -0400

    Update Audio UI.
    Created prefab of Audio UI and moved it to the Common Scripts folder and namespace.

---
+++ Assets/Problems/Common Scripts/AudioUI.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace CommonScripts
+{
+    public class AudioUI : MonoBehaviour
+    {
+        [SerializeField]
+        private UnityEngine.Audio.AudioMixer _mainMixer = null;
+
+        [SerializeField]
+        private UnityEngine.UI.Slider _slider = null;
+
+        private void Awake()
+        {
+            _slider.value = .5f;
+            _slider.onValueChanged.AddListener(OnSliderChange);
+            UpdateSound(_slider.value);
+        }
+
+        public void OnSliderChange(float value)
+        {
+            UpdateSound(value);
+        }
+
+        private void UpdateSound(float value)
+        {
+            if (value > 0f)
+                _mainMixer.SetFloat("MasterVolume", Mathf.Lerp(-30f, -15f, value));
+            else
+                _mainMixer.SetFloat("MasterVolume", -80f);
+        }
+    }
+}
